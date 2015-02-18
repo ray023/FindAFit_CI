98187920456c60e869b870c2482bb7a2372d934b
Ray Nowell <ray023@gmail.com>
2015-02-17 20:36:08 -0600

    more adjustments

--- application/controllers/feedback.php
+++ application/controllers/feedback.php
@@ -10,6 +10,55 @@ class Feedback extends CI_Controller {
 
         return;
     }
+    
+    public function submit_feedback()
+    {
+            define('USER_FEEDBACK',3);
+            define('CORDOVA',4);
+            define('MODEL',5);
+            define('PLATFORM',6);
+            define('UUID', 7);
+            define('VERSION', 8);
+
+            $this->load->model('Find_a_fit_model');
+
+            $feedback_count =   $this->Find_a_fit_model->get_count_of_todays_feedback();
+
+            if ($feedback_count > 500)
+            {
+                    echo 'Maximum feedback reached.  Try again tomorrow.';
+                    return;
+            }
+
+            $data = array(
+                    'ip_address'  =>   $_SERVER['REMOTE_ADDR'],
+                    'user_feedback' =>  $this->uri->segment(USER_FEEDBACK),
+                    'device_info_cordova' =>  $this->uri->segment(CORDOVA),
+                    'device_info_model' =>  $this->uri->segment(MODEL),
+                    'device_info_platform' =>  $this->uri->segment(PLATFORM),
+                    'device_info_uuid' =>  $this->uri->segment(UUID),
+                    'device_info_version' =>  $this->uri->segment(VERSION),
+             );
+
+            $feedback_count =   $this->Find_a_fit_model->save_feedback($data);
+
+            echo 'Feedback submitted successfully.  Thank-you!';
+
+            $this->load->library('email');
+            $config['protocol']		=	'mail';
+            $config['charset']		=	'iso-8859-1';
+            $config['mailtype']		=	'html';
+            $this->email->initialize($config);
+            $this->email->from('ray@wod-minder.com', 'WOD-Minder Admin');
+            $this->email->to('ray023@gmail.com');
+            $this->email->subject('Find A Fit Feedback');
+            $this->email->message($this->uri->segment(USER_FEEDBACK));
+            $this->email->send();
+
+            return;
+
+    }
+
 }
 
 /* End of file feedback.php */
--- application/controllers/gps.php
+++ application/controllers/gps.php
@@ -10,7 +10,118 @@ class Gps extends CI_Controller {
 
         return;
     }
+    
+    public function get_boxes()
+    {
+        define('APP_SOURCE'		,3);
+        define('INFO_SOURCE'	,4);
+        define('RESULT_COUNT'	,5);
+        define('ADDRESS_VALUE'	,6);
+        define('LATITUDE'		,6);
+        define('LONGITUDE'		,7);
+
+        $app_source	=   $this->uri->segment(APP_SOURCE);
+        $info_source	=   $this->uri->segment(INFO_SOURCE);
+        $latitude	=   0;
+        $longitude	=   0;
+        $address_value	=   '';
+        $return_value	=   '';
+        $results	=   '';
+        $result_count	=   0;
+
+        $this->load->model('Find_a_fit_model');
+
+        if ($info_source === 'current_position')
+        {
+            $latitude	=   $this->uri->segment(LATITUDE);
+            $longitude	=   $this->uri->segment(LONGITUDE);
+
+            $stat_data['ip_address']    =   $_SERVER['REMOTE_ADDR'];
+            $stat_data['faf_source']    =   $app_source;
+            $stat_data['latitude']      =   $latitude;
+            $stat_data['longitude']     =   $longitude;
+            $this->Find_a_fit_model->save_stat($stat_data);
+
+        }
+        elseif ($info_source === 'address_field')
+        {
+            $address_value  =   $this->uri->segment(ADDRESS_VALUE);
+
+            $stat_data['ip_address']    =   $_SERVER['REMOTE_ADDR'];
+            $stat_data['faf_source']    =   $app_source;
+            $stat_data['search_term']   =   $address_value;
+            $this->Find_a_fit_model->save_stat($stat_data);
+
+            $return_value   =   file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?address='.$address_value.'&key=AIzaSyBecnfstyx5PtF6nJaieEhlP3DpCwTRTzU');
+            $json = json_decode($return_value, true);
+
+            if ($json["status"] === "OK" )
+            {
+                if (count($results) > 1)
+                    echo '<span class="error-messsage">WARNING:  Address info too broad.</span>';
+
+                $results =  $json["results"];
+                $geom =  $results[0]["geometry"];
+                $location = $geom["location"];
+                $latitude = $location["lat"];
+                $longitude = $location["lng"];
+            }
+            else
+            {
+                //START AUDIT
+                $this->load->model('Audit_model');
+                $audit_data['controller']	=	'find_a_fit_mobile_with_options';
+                $audit_data['ip_address']	=	$_SERVER['REMOTE_ADDR'];
+                $audit_data['member_id']	=	$this->session->userdata('member_id');
+                $audit_data['member_name']	=	$this->session->userdata('display_name');
+                $audit_data['short_description']	=	'Map Error';
+                $audit_data['full_info']	=	$json["status"];
+                $this->Audit_model->save_audit_log($audit_data);
+                //END AUDIT
+                echo '<span class="error-messsage">MAP ERROR:  '.$json["status"].'</span>';
+                return;
+            }
+        }
+        else
+        {
+            echo 'Source '.$info_source.' is not defined.';
+            return;
+        }
+
+        $result_count = $this->uri->segment(RESULT_COUNT);
+
+        if (is_numeric($result_count) && $result_count <= 50)
+        {
+            //we are good
+        }
+        else
+            $result_count = 5;
+
+        $full_info = $latitude.','.$longitude;
+
+        $this->load->model('Find_a_fit_model');
+        $facilities_array	=	$this->Find_a_fit_model->get_closest_crossfits($latitude, $longitude, $result_count);
+
+        echo '<div class = "ui-grid-b">';
+        echo	'<div class="ui-block-a mobile-grid-header " style = "height:60px;text-align: center;">Affiliate</div>'.
+                        '<div class="ui-block-b mobile-grid-header " style = "height:60px;text-align: center;">Distance<br>(approx.)</div>'.
+                        '<div class="ui-block-c mobile-grid-header"  style = "height:60px;text-align: center;">Directions</div>';
+        foreach($facilities_array as $row) 
+        {
+                $google_search_affiliate	=	'<a href="#" OnClick="window.open(\'https://www.google.com/?gws_rd=ssl#q='.str_replace(' ','+',$row['affil_name']).'\', \'_system\', \'\');" data-ajax="false">'.$row['affil_name'].'</a>';
+                $box = (is_null($row['url']) | $row['url'] === '') ? $google_search_affiliate : '<a href="#" OnClick="window.open(\''.$row['url'].'\', \'_system\', \'\');" data-ajax="false">'.$row['affil_name'].'</a>';
+
+                echo '<div class = "ui-block-a"><div class = "ui-bar ui-bar-a" style = "height:60px;text-align: center;">'.$box.'</div></div>';
+                echo '<div class = "ui-block-b "><div class = "ui-bar ui-bar-a" style = "height:60px;text-align: center;">'.$row['distance'].'</div></div>';
+                echo '<div class = "ui-block-c number-block"><div class = "ui-bar ui-bar-a" style = "height:60px;text-align: center;"><a href="#" OnClick="window.open(\'http://maps.google.com/?saddr='.$latitude.','.$longitude.'&daddr='.$row['latitude'].','.$row['longitude'].'\', \'_system\', \'\');" data-ajax="false" class="ui-btn ui-icon-navigation ui-btn-icon-notext ui-corner-all" >Navigate</a></div></div>';
+
+                $full_info .= '|'.$row['affil_name'];
+        }
+        echo '</div><!--/grid-a-->';
+
+    }
+
 }
 
-/* End of file feedback.php */
+/* End of file gps.php */
 /* Location: ./application/controllers/gps.php */
