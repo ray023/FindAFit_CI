c13ef9e035b77ddf65ad731c7ef63f23d0c2dcc3
Ray Nowell <ray023@gmail.com>
2015-03-17 14:52:15 -0500

    new look

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -31,33 +31,63 @@
           <h4>Free</h4>
           <p>No cost; no ads.</p>
         </div>
-        <div class="col-lg-12">
+        <div class="col-lg-6">
           <h4>Sleek</h4>
             <p>Modern look and feel across iOS and Android devices.</p>
-            <div class="row">
-                <div class="col-md-3">
-                    <a href="<?php echo base_url().'images/01_news.jpg';?>" class="thumbnail">
-                        <img src="<?php echo base_url().'images/01_news.jpg';?>" alt="News">
-                    </a>
+            
+            <!--ray start-->
+            <div id="faf-screenshots" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                  <li data-target="#faf-screenshots" data-slide-to="0" class="active"></li>
+                  <li data-target="#faf-screenshots" data-slide-to="1"></li>
+                  <li data-target="#faf-screenshots" data-slide-to="2"></li>
+                </ol>
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+                  <div class="item active">
+                    <img src="<?php echo base_url().'images/01_news.jpg';?>" alt="News">
+                    <div class="carousel-caption">
+                      Stay informed
+                    </div>
+                  </div>
+                  <div class="item">
+                    <img src="<?php echo base_url().'images/02_gps.jpg';?>" alt="GPS">
+                    <div class="carousel-caption">
+                      Nearest boxes by your current location
+                    </div>
+                  </div>
+
+                  <div class="item">
+                    <img src="<?php echo base_url().'images/03_address.jpg';?>" alt="Details">
+                    <div class="carousel-caption">
+                      Detailed box info
+                    </div>
+                  </div>
+                  <div class="item">
+                    <img src="<?php echo base_url().'images/02_gps.jpg';?>" alt="Address">
+                    <div class="carousel-caption">
+                      Search by an address
+                    </div>
+                  </div>
                 </div>
-                <div class="col-md-3">
-                    <a href="<?php echo base_url().'images/02_gps.jpg';?>" class="thumbnail">
-                        <img src="<?php echo base_url().'images/02_gps.jpg';?>" alt="News">
-                    </a>
-                </div>
-                <div class="col-md-3">
-                    <a href="<?php echo base_url().'images/03_address.jpg';?>" class="thumbnail">
-                        <img src="<?php echo base_url().'images/03_address.jpg';?>" alt="News">
-                    </a>
-                </div>
-                <div class="col-md-3">
-                    <a href="<?php echo base_url().'images/04_address.jpg';?>" class="thumbnail">
-                        <img src="<?php echo base_url().'images/04_address.jpg';?>" alt="News">
-                    </a>
-                </div>
-            </div>
+
+                <!-- Controls -->
+                <a class="left carousel-control" href="#faf-screenshots" role="button" data-slide="prev">
+                  <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#faf-screenshots" role="button" data-slide="next">
+                  <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                  <span class="sr-only">Next</span>
+                </a>
+              </div>
+            <!-- ray finish-->
+            
+            
         </div>
-          <div class="col-lg-12">
+          <div class="col-lg-6">
           <h4>What others are saying</h4>
           <blockquote class="bg-info">
               <p>It's the best find a gym app by far. It works as intended quickly.</p>
