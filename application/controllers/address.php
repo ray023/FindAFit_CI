bb6eb2b9f8159848e2758deb1833c7c73fbba0f9
Ray Nowell <ray023@gmail.com>
2015-03-27 11:03:46 -0500

    new method to return start position for center map

--- application/controllers/address.php
+++ application/controllers/address.php
@@ -11,6 +11,127 @@ class Address extends CI_Controller {
         return;
     }
     
+    public function get_json_with_start_position()
+    {
+        define('ADDRESS_VALUE'	,3);
+        define('RESULT_COUNT'	,4);
+
+        $address_value	=   '';
+        $return_value	=   '';
+        $results	=   '';
+        $result_count	=   0;
+
+        $this->load->model('Affiliates_model');
+        $this->load->model('Stats_model');
+        $this->load->model('Audit_model');
+        
+        $address_value   =   $this->uri->segment(ADDRESS_VALUE);
+        $result_count   =   $this->uri->segment(RESULT_COUNT);
+
+        $stat_data['ip_address']    =   $_SERVER['REMOTE_ADDR'];
+        $stat_data['faf_source']    =   'address';
+        $stat_data['search_term']   =   $address_value;
+        $stat_data['result_count']  =   $result_count;
+        $this->Stats_model->save_stat($stat_data);
+
+        $return_value   =   file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?address='.$address_value.'&key=AIzaSyBecnfstyx5PtF6nJaieEhlP3DpCwTRTzU');
+        $json = json_decode($return_value, true);
+        
+        $start_position = false;
+        if ($json["status"] === "OK" )
+        {
+            if (count($results) > 1)
+            {
+                $audit_data['controller']	=   'address';
+                $audit_data['ip_address']	=   $_SERVER['REMOTE_ADDR'];
+                $audit_data['short_description']=   'Map Warning';
+                $audit_data['full_info']	=   'Address info too broad for '.$address_value;
+                $this->Audit_model->save_audit_log($audit_data);
+            }
+
+            $results =  $json["results"];
+            $geom =  $results[0]["geometry"];
+            $location = $geom["location"];
+            $latitude = $location["lat"];
+            $longitude = $location["lng"];
+            
+            $start_position = array(
+                'search_term' => $address_value,
+                'latitude' => $latitude,
+                'longitude' => $longitude,
+            );
+        }
+        else
+        {
+            
+            $value = urldecode($address_value);
+            $value = str_replace("_OPEN_PARENTHESIS_","(",$value);
+            $value = str_replace("_CLOSE_PARENTHESIS_",")",$value);
+            $value = str_replace("_HYPHEN_","-",$value);
+            $value = str_replace("_PERIOD_",".",$value);
+            $value = str_replace("_EXCLAMATION_MARK_","!",$value);
+            $value = str_replace("_TILDE_","~",$value);
+            $value = str_replace("_ASTERISK_","*",$value);
+            $value = str_replace("_APOSTROPHE_","'",$value);
+            $value = str_replace("_COLON_",":",$value);
+            $value = str_replace("_SEMICOLON_",";",$value);
+            $value = str_replace("_AT_SIGN_","@",$value);
+            $value = str_replace("_AMPERSAND_","&",$value);
+            $value = str_replace("_DOUBLE_QUOTE_","\"",$value);
+            $value = str_replace("_PERCENT_","%",$value);
+            $value = str_replace("_QUESTION_","?",$value);
+            $value = str_replace("_COMMA_",",",$value);
+            $value = str_replace("_BACKSLASH_","\\",$value);
+            $value = str_replace("_SLASH_","/",$value);
+            $value = str_replace("_DOLLAR_SIGN_","$",$value);
+            
+            $affil_by_name = false;
+            if ($json["status"] === 'ZERO_RESULTS')
+            {
+                //User might be trying to find affiliate by name.  Search the databae
+                $affil_by_name  =   $this->Affiliates_model->get_affiliate_by_name($value);   
+            }
+            
+            if (!!$affil_by_name)
+            {
+                $latitude   =   $affil_by_name['latitude'];
+                $longitude  =   $affil_by_name['longitude'];
+            }
+            else
+            {
+                $this->load->model('Audit_model');
+                $audit_data['controller']	=	'address';
+                $audit_data['ip_address']	=	$_SERVER['REMOTE_ADDR'];
+                $audit_data['short_description']	=	'Map Error';
+                $audit_data['full_info']	=	$json["status"].' on \''.$address_value.'\'';
+                $this->Audit_model->save_audit_log($audit_data);
+                return;
+            }                    
+        }
+
+
+        $result_count = $this->uri->segment(RESULT_COUNT);
+
+        if (is_numeric($result_count) && $result_count <= 50)
+        {
+            //we are good
+        }
+        else
+            $result_count = 5;
+
+        $affiliate_list	=	$this->Affiliates_model->get_affiliates_by_location($latitude, $longitude, $result_count);
+        
+        $return_array = Array(
+            'start_position' => $start_position,
+            'affil_list' => $affiliate_list,
+        );
+        
+        $this->output
+            ->set_content_type('application/json')
+            ->set_output(json_encode($return_array));
+
+    }
+    
     public function get_json()
     {
         define('ADDRESS_VALUE'	,3);
