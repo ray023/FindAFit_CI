e23a2c7c6759700f4ba7be867e628c11c4ca3cdf
Ray Nowell <Ray Nowell>
2017-03-21 11:21:07 -0500

    upgraded

--- application/controllers/getupgraded.php
+++ application/controllers/getupgraded.php
@@ -11,11 +11,25 @@ class Getupgraded extends CI_Controller {
 	}
         public function cancelled()
         {
-            echo 'aw man, you canncled';
+            $this->load->model('Audit_model');
+            
+            $audit_data['controller']	=   'Upgrade Cancelled';
+            $audit_data['ip_address']	=   $_SERVER['REMOTE_ADDR'];
+            $audit_data['short_description']=   'User clicked on PayPal upgrade but cancelled';
+            $this->Audit_model->save_audit_log($audit_data);
+            
+            echo 'No problem.  Find A Fit is here for you should you change you mind.  If you have questions/concerns, use Find A Fits Feedback feature';
         }
         public function success()
         {
-            echo 'yahoo';
+            $this->load->model('Audit_model');
+            
+            $audit_data['controller']	=   'Upgrade Success';
+            $audit_data['ip_address']	=   $_SERVER['REMOTE_ADDR'];
+            $audit_data['short_description']=   'User clicked on PayPal upgrade and paid';
+            $this->Audit_model->save_audit_log($audit_data);
+            
+            echo 'All right!  You will be contacted within 24 hours to get the details required for your upgrade.  Thank-you!';
         }
 }
 
