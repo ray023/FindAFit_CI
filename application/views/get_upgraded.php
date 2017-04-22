8cec8c3c9e65a7d2a785c6e603f934e7f4777a3e
Ray Nowell <Ray Nowell>
2017-04-22 10:19:07 -0500

    update text

--- application/views/get_upgraded.php
+++ application/views/get_upgraded.php
@@ -3,40 +3,21 @@
     <p>With thousands of downloads and daily use across the globe, let Find-A-Fit help bring in the business.</p>
     <p>Choose between two upgraded listings</p>
     <div class="panel panel-info">
-      <div class="panel-heading">Drop-In Friendly ($3.99)</div>
+      <div class="panel-heading">Drop-In Friendly ($0.99)</div>
       <div class="panel-body">
-        <p>For the cost of an app, you can list your drop-in rate, drop-in link, a message to potential customers and all your Social Media links. Your box will be featured as "Drop-In Friendly" in the results.</p>
+        <p>For the cost of a cheap app, you can list your drop-in rate, drop-in link, a message to potential customers and all your Social Media links. Your box will be featured as "Drop-In Friendly" in the results.</p>
       </div>
     </div>
     <div class="panel panel-info">
-      <div class="panel-heading">Preferred ($39.99)</div>
+      <div class="panel-heading">Preferred ($2.99)</div>
       <div class="panel-body">
-          <p>For the cost of a drop-in, you get all the benefits of Drop-In Friendly AND your box will always be listed <strong>FIRST</strong> for potential clients!</p>
+          <p>For the cost an app, you get all the benefits of Drop-In Friendly AND your box will always be listed <strong>FIRST</strong> for drop-ins and potential clients!</p>
       </div>
     </div>
-        
-    
-    <div>
-        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-            <input type="hidden" name="cmd" value="_s-xclick">
-            <input type="hidden" name="hosted_button_id" value="A7UB24GYTYXJQ">
-            <table>
-            <tr>
-                <td><input type="hidden" name="on0" value="Enhance Box">Enhance Listing</td>
-            </tr>
-            <tr>
-                <td>
-                    <select name="os0">
-                        <option value="Drop-In Friendly">Drop-In Friendly $3.99 USD</option>
-                        <option value="Preferred Listing">Preferred Listing $39.99 USD</option>
-                    </select> 
-                </td>
-            </tr>
-            </table>
-            <input type="hidden" name="currency_code" value="USD">
-            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-            <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-            </form>
-
-    </div>
+    <p>
+        <a href="https://www.paypal.me/o1solution/0.99" class="btn btn-info">Drop-In Friendly ($0.99)</a>
+    </p>
+    <p>
+        <a href="https://www.paypal.me/o1solution/2.99" class="btn btn-info">PREFERRED ($2.99)</a>
+    </p>
 </div>
